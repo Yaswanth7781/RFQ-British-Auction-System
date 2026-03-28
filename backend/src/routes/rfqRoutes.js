566ab511c8fcef6be4ee16f3e0c74d23ec00a34e
Yaswanth7781 <149923270+Yaswanth7781@users.noreply.github.com>
2026-03-28 18:26:21 +0530

    Update rfqRoutes.js

--- backend/src/routes/rfqRoutes.js
+++ backend/src/routes/rfqRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const rfqController = require("../controllers/rfqController");
 
+
 router.post("/", rfqController.createRFQ);
 router.post("/create", rfqController.createRFQ);
+
+router.get("/", rfqController.getRFQs);
 
 module.exports = router;
